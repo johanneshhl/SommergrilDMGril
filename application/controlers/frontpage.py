f7db847c6cce3b8bc4a1b87a569607f19cb42966
Johannes Hvilsom Larsen <hvilsom@outlook.com>
2015-06-24 12:57:25 +0200

    first commit

--- application/controlers/frontpage.py
+++ application/controlers/frontpage.py
@@ -2,6 +2,7 @@
  # -*- coding: utf-8 -*-
 
 from application import app, request, redirect, escape, session, url_for, db, render_template, g, flash
+from application.database.database import userContact, newsLetterEmail
 
 @app.route('/')
 def index():
@@ -69,3 +70,47 @@ def bestil():
 	'''
 
 	return render_template('bestil.html')
+
+
+@app.route('/bestil/download')
+def bestilDownload():
+
+	if 'documents' in session:
+		return render_template('pdfer.html')
+
+	else: 
+		return redirect(url_for('bestil'))
+
+
+
+
+
+
+@app.route('/session/signup', methods=['POST'])
+def signup():
+
+	newUser = newsLetterEmail(request.form['email']) #opret bruger
+		
+	#indsæt User Objectet
+	db.session.add(newUser)
+	db.session.commit()
+
+	return redirect(url_for('index'))
+
+
+
+@app.route('/session/order', methods=['POST'])
+def order():
+
+	newUser = userContact(request.form['name'],request.form['email'],request.form['phonenumber']) #opret bruger
+		
+	#indsæt User Objectet
+	db.session.add(newUser)
+	db.session.commit()
+
+
+	session['documents'] = True
+	session.permanent = True
+
+
+	return redirect(url_for('bestilDownload'))
